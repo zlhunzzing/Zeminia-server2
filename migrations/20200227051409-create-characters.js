18dbd643d3c2334be3d3509a6db48087457587b4
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-02-27 14:35:03 +0900

    sequelize modeling 완료
    users, characters, chats, monsters, items, monsters_items 모델링 완료 되었습니다.

---
+++ migrations/20200227051409-create-characters.js
@@ -0,0 +1,66 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('characters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      level: {
+        type: Sequelize.INTEGER
+      },
+      character_name: {
+        type: Sequelize.STRING
+      },
+      maxHp: {
+        type: Sequelize.INTEGER
+      },
+      hp: {
+        type: Sequelize.INTEGER
+      },
+      att: {
+        type: Sequelize.INTEGER
+      },
+      def: {
+        type: Sequelize.INTEGER
+      },
+      exp: {
+        type: Sequelize.INTEGER
+      },
+      rankScore: {
+        type: Sequelize.INTEGER
+      },
+      gold: {
+        type: Sequelize.INTEGER
+      },
+      mp: {
+        type: Sequelize.INTEGER
+      },
+      weapon: {
+        type: Sequelize.STRING
+      },
+      login_time: {
+        type: Sequelize.DATE
+      },
+      logout_time: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('characters');
+  }
+};
