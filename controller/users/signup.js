d4279092ffbb1c06a2c869f14b229fdc3c0c75cd
Dal-ya <hyansky@daum.net>
2020-02-28 01:53:17 +0900

    signup email, password 값 검증 ----------------- 회원가입시 이메일, 패스워드에대한 값 검증 추가 매우 엄격하고 완벽한 것이 아닌 간략한 버전. 패스워드 규칙: 8자, 소문자, 숫자, 특수문자 각 하나 이상 조합 레퍼런스: http://blog.naver.com/PostView.nhn?blogId=yjhyjh5369&logNo=221289465679&parentCategoryNo=&categoryNo=16&viewDate=&isShowPopularPosts=true&from=search

--- controller/users/signup.js
+++ controller/users/signup.js
@@ -1,12 +1,35 @@
-const { users } = require('../../models');
 const bcrypt = require('bcrypt');
+const { users } = require('../../models');
 
 module.exports = {
   post: async (req, res, next) => {
     try {
       const { email, password } = req.body;
-      //* need email, password validation check
-      // Todo: email, password check
+      //* email, password validation check
+      if (email === '' || password === '') {
+        res.status(409).json({ failed: '값을 정확하게 입력해주세요' });
+        return;
+      }
+
+      if (email.includes(' ') || password.includes(' ')) {
+        res.status(409).json({ failed: '공백은 허용하지 않습니다' });
+        return;
+      }
+
+      const regExpEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+      if (email.match(regExpEmail) === null) {
+        res.status(409).json({ failed: 'email 형식에 맞게 값을 넣어주세요' });
+        return;
+      }
+
+      const regExpPw = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+      if (regExpPw.test(password) === false) {
+        res
+          .status(409)
+          .json({ failed: '8자 이상, 소문자, 숫자, 특수문자 필요합니다' });
+
+        return;
+      }
 
       const hash = bcrypt.hashSync(password, 8);
       const user = await users.findAll({ where: { email } });
@@ -23,6 +46,7 @@ module.exports = {
         signupCheck: 'success'
       });
     } catch (err) {
+      // eslint-disable-next-line no-console
       console.log(err);
       next();
     }
