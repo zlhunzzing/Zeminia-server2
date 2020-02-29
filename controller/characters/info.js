4257111f40665caf75636e315917c11cc68c923d
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-02-29 20:53:39 +0900

    /charactesr/info API 완료
    기존 캐릭터의 정보를 보내주는 API 입니다~

--- controller/characters/info.js
+++ controller/characters/info.js
@@ -1,5 +1,21 @@
 const { characters } = require('../../models');
 
 module.exports = {
-  get: async (req, res, next) => {}
+  get: async (req, res, next) => {
+    try {
+      if (req.session.userId) {
+        const info = await characters.findAll({
+          where: { user_id: req.session.userId }
+        });
+        res.status(200).send(info[0].dataValues);
+      } else {
+        // 세션아이디 없음. 불가능한 접근
+        res.status(401).send({ failed: '인증되지 않은 사용자 입니다.' });
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      next();
+    }
+  }
 };
