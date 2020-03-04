d231feb1aab4fd9930707d25db765a0581a8388e
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-03-04 12:53:16 +0900

    사소한 부분 수정
    사소한 코드 간결 변경이 2군데 있습니다 ㅎㅎ

--- migrations/20200227051414-create-chats.js
+++ migrations/20200227051414-create-chats.js
@@ -14,6 +14,9 @@ module.exports = {
       message: {
         type: Sequelize.STRING
       },
+      roomname: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/chats.js
+++ models/chats.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       // 이모지도 저장할 수 있게 설정하기.
       timestamps: true,
-      paranoid: true,
+      // paranoid: true,
       charset: 'utf8mb4',
       collate: 'utf8mb4_unicode_ci'
     }
